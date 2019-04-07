fdca56dc2ee622f537c4b9c7966454eee3b82f8d
Suguru Oki <oki.suguru@hamee.co.jp>
2019-04-08 00:15:59 +0900

    [追加] apiのメインファイルを追加

---
+++ app/main.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify, request
+import json
+app = Flask(__name__)
+
+@app.route("/", methods=['GET'])
+def hello():
+    return "root get. Hello!"
+
+@app.route('/reply', methods=['GET'])
+def reply():
+    answer = "route reply is got! \n"
+    result = {
+      "Content-Type": "application/json",
+      "Answer":{"Text": answer}
+    }
+    return jsonify(result)
+
+@app.route('/reply', methods=['POST'])
+def reply():
+    data = json.loads(request.data)
+    answer = "route post. keyword is %s!\n" % data["keyword"]
+    result = {
+      "Content-Type": "application/json",
+      "Answer":{"Text": answer}
+    }
+    return jsonify(result)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=50000,debug=True)
+
+
