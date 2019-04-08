87db10d48834d6eece592baf2024f014038d3f2e
Suguru Oki <oki.suguru@hamee.co.jp>
2019-04-08 17:01:12 +0900

    [追加] main.pyから余計な記述を削除

--- app/main.py
+++ app/main.py
@@ -6,15 +6,6 @@ app = Flask(__name__)
 def hello():
     return "root get. Hello!"
 
-# @app.route('/reply', methods=['GET'])
-# def reply():
-#     answer = "route reply is got! \n"
-#     result = {
-#       "Content-Type": "application/json",
-#       "Answer":{"Text": answer}
-#     }
-#     return jsonify(result)
-
 @app.route('/reply', methods=['POST'])
 def reply():
     data = json.loads(request.data)
